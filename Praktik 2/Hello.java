38cac7cbaf5b1e14a91ca58baf101c8d21e08983
Arfiko <fikocool@gmail.com>
2021-09-14 17:15:13 +0700

    menambahkan comment NIM dan Nama

--- Praktik 2/Hello.java
+++ Praktik 2/Hello.java
@@ -1,4 +1,9 @@
+/*
+NIM : 21537144034
+Nama : Arfiko Dandys Sutanto
+*/
 public class Hello {
+    //Untuk menampilkan teks
     public static void main(String []args) {
         System.out.println("Hello,worlds!");
     }
