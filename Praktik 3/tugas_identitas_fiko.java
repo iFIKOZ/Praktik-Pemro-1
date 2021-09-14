05b009b5103048836f621d847e0f6c923fb82eea
Arfiko <fikocool@gmail.com>
2021-09-14 18:56:51 +0700

    tugas identitas java dan python

---
+++ Praktik 3/tugas_identitas_fiko.java
@@ -0,0 +1,27 @@
+public class tugas_identitas_fiko {
+    public static void main(String[] args){
+        String namaDepan = "Arfiko";
+        String namaTengah = "Dandys";
+        String namaBelakang = "Sutanto";
+        String namaPanggilan = "FIKO";
+        String nim = "21537144034";
+        int angkatan = 2021;
+        String programStudi = "S1-Teknologi Informasi";
+        String jurusan = "JPTEI";
+        String fakultas = "Fakultas Teknik";
+        double ipk = 3.4;
+
+        System.out.println("||~~~~Identitas Mahasiswa UNY~~~~||");
+        System.out.println("");
+        System.out.println("1. Nama Lengkap     : " + namaDepan + " " + namaTengah + " " + namaBelakang);
+        System.out.println("2. Nama Panggilan   : " + namaPanggilan);
+        System.out.println("3. NIM              : " + nim);
+        System.out.println("4. Tahun Angkatan   : " + angkatan);
+        System.out.println("5. Program Studi    : " + programStudi);
+        System.out.println("6. Jurusan          : " + jurusan);
+        System.out.println("7. Fakultas         : " + fakultas);
+        System.out.println("8. IPK Harapan      : >" + ipk);
+        System.out.println("");
+
+    }
+}
---
+++ Praktik 3/tugas_identitas_fiko.py
@@ -0,0 +1,23 @@
+namaDepan = "Arfiko"
+namaTengah = "Dandys"
+namaBelakang = "Sutanto"
+namaPanggilan = "FIKO"
+nim = "21537144034"
+angkatan = 2021
+programStudi = "S1-Teknologi Informasi"
+jurusan = "JPTEI"
+fakultas = "Fakultas Teknik"
+ipkHarapan = 3.4
+
+print("||~~~~Identitas Mahasiswa UNY~~~~||")
+print("")
+print("1. Nama Lengkap     : " + namaDepan + " " + namaTengah +" "+ namaBelakang)
+print("2. Nama Panggilan   : " + namaPanggilan)
+print("3. NIM              : " + nim)
+print("4. Tahun Angkatan   : " + str(angkatan))
+print("5. Program Studi    : " + programStudi)
+print("6. Jurusan          : " + jurusan)
+print("7. Fakultas         : " + fakultas)
+print("8. IPK Harapan      : " + str(ipkHarapan))
+print("")
+
