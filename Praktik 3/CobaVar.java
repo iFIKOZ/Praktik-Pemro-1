511f69866da517f117734df8b80265f28a759945
Arfiko <fikocool@gmail.com>
2021-09-14 14:11:52 +0700

    first commit

---
+++ Praktik 3/CobaVar.java
@@ -0,0 +1,14 @@
+public class CobaVar {
+    public static void main(String [] args){
+        String namaDepan = "Arfiko";
+        String namaBelakang = "Sutanto";
+        int umur = 18;
+        double tinggiBadan = 165;
+        double beratBadan = 55.5;
+
+        System.out.println("Nama : " + namaDepan + " " + namaBelakang);
+        System.out.println("Umur : " + umur + " tahun");
+        System.out.println("Tinggi Badan : " + (int)tinggiBadan + " cm");
+        System.out.println("Berat Badan : " + beratBadan + " kg");
+    }
+}
---
+++ Praktik 3/CobaVar.py
@@ -0,0 +1,10 @@
+namaDepan = "Arfiko"
+namaBelakang = "Sutanto"
+umur = 18
+tinggiBadan = 1.65
+beratBadan = 55.5
+
+print("Nama saya adalah " + namaDepan + namaBelakang)
+print("Umur saya adalah " + str(umur) + " tahun")
+print("Tinggi badan saya adalah " + str(tinggiBadan) + " m")
+print("Tinggi badan saya adalah " + str(beratBadan) + " kg")
