d2958ee120ab212e52d23bdfd3d39ba837a48542
Arfiko <fikocool@gmail.com>
2021-10-28 19:54:34 +0700

    praktik 8

---
+++ Praktik 8/BelajarTanpaArray.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BelajarTanpaArray {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Masukkan bilangan ke-1 : ");
+        int bilangan1 = scanner.nextInt();
+        System.out.print("Masukkan bilangan ke-2 : ");
+        int bilangan2 = scanner.nextInt();
+        System.out.print("Masukkan bilangan ke-3 : ");
+        int bilangan3 = scanner.nextInt();
+
+        int jumlah = bilangan1 + bilangan2 + bilangan3;
+        System.out.println("Jumlah = " + jumlah);
+    }
+}
